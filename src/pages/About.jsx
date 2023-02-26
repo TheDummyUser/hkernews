848dec09b24a324f442b14fc36387b38ddd9c941
grim909 <abhiram.reddy122002@gmail.com>
2023-02-26 11:06:08 +0530

    added loading gif edited about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,20 +3,19 @@ import {FiHeart} from 'react-icons/fi';
 const About = () => {
   return (
     <div className='font-mono grid p-5'>
-      <p className='text-3xl'>about:</p>
-      <div className='mt-5'>
-        <div className=''> {/* want to push the last text to the end as a footer. */}
-        <p>
-          Hacker News is a social news website focused on technology and entrepreneurship. It was created by the startup accelerator Y Combinator and is run by a small team of moderators. Users can submit links to articles, blog posts, or other online content related to technology, startups, programming, or other related topics. The content is then ranked based on user votes and comments, with the most popular content appearing at the top of the page. In addition to the front page, there are also sub-pages for jobs, ask and show where users can ask questions or showcase their projects. Hacker News is known for its engaged and knowledgeable community and has become a popular source of news and discussion for people in the tech industry.
-        </p>
-        <p className='grid place-items-center p-5'><a href="http://news.ycombinator.com">vist the official site.</a></p>
-        </div>
-        <footer className='grid place-content-center'>
-        <p className='flex items-center'>made with <FiHeart/> by abhiram</p>
-      </footer>
-      </div>
+  <p className='text-3xl'>about:</p>
+  <div className=''>
+    <div className='mt-10'>
+      <p>
+        Hacker News is a social news website focused on technology and entrepreneurship. It was created by the startup accelerator Y Combinator and is run by a small team of moderators. Users can submit links to articles, blog posts, or other online content related to technology, startups, programming, or other related topics. The content is then ranked based on user votes and comments, with the most popular content appearing at the top of the page. In addition to the front page, there are also sub-pages for jobs, ask and show where users can ask questions or showcase their projects. Hacker News is known for its engaged and knowledgeable community and has become a popular source of news and discussion for people in the tech industry.
+      </p>
+      <p className='grid place-items-center p-5'><a href="http://news.ycombinator.com">vist the official site.</a></p>
     </div>
-
+  </div>
+  <footer className='fixed bottom-0 w-full grid place-items-center py-2'>
+    <p className='text-center flex items-center'>made with <FiHeart className='text-red-500 mx-1'/> by abhiram</p>
+  </footer>
+</div>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -38,7 +38,13 @@ const Home = () => {
     <div className='font-mono'>
       <h1 className='m-10 font-mono text-xl'>Top Stories</h1>
       {isLoading ? (
-        <p>Loading...</p>
+        <div className="flex justify-center items-center h-screen">
+        <img 
+          src="/src/assets/kakaotalk-emoticon.gif" 
+          alt="Loading..." 
+          className="w-24 h-24 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" 
+        />
+      </div>
       ) : (
         <div>
           {data.map(story => (
