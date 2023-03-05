7e4e17850138f3280f0e5d7ff091583a3930430b
grim909 <abhiram.reddy122002@gmail.com>
2023-03-05 21:40:33 +0530

    none

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -41,7 +41,7 @@ const Home = () => {
       {isLoading ? (
         <div className="flex justify-center items-center h-screen">
         <img 
-          src={kakaotalkEmoticon} alt="KakaoTalk emoticon"
+          src={kakaotalkEmoticon} alt=""
           className="w-24 h-24 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" 
         />
       </div>
